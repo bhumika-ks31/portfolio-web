9ed7cde2b1b74079e931be39e0b0e92d0b87a195
Bhumika kashyap <tilantajagra@gmail.com>
2025-09-26 19:52:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: process.env.PORT || 4173,      // Render ke liye dynamic PORT
+    host: true,                           // 0.0.0.0 pe bind kare
+    allowedHosts: ['portfolio-web-3-tgt3.onrender.com']  // Render host allow
+  }
 })
